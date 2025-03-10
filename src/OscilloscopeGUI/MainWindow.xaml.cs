4aefb752e7827617a63f37713ba5925f78c5f398
Ondřej Jócsik <Ondrej.Jocsik@seznam.cz>
2025-03-10 10:36:18 +0100

    oprava vizualizace dat

--- src/OscilloscopeGUI/MainWindow.xaml.cs
+++ src/OscilloscopeGUI/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;  // PRIDANO - pro File.Exists()
+using System.IO;
 using System.Linq;
 using System.Windows;
 using Microsoft.Win32;
 using ScottPlot;
-using OscilloscopeCLI.Data; 
+using OscilloscopeCLI.Data;
 
 namespace OscilloscopeGUI {
     public partial class MainWindow : Window {
@@ -27,38 +27,42 @@ namespace OscilloscopeGUI {
                     await Task.Run(() => loader.LoadCsvFile(openFileDialog.FileName));
 
                     if (loader.SignalData.Count == 0) {
-                        MessageBox.Show("Soubor neobsahuje zadna platna data.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        MessageBox.Show("Soubor neobsahuje platna data.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Warning);
                         return;
-                    }
-
-                    // Automaticke nalezeni odpovidajiciho TXT souboru
-                    string csvFilePath = openFileDialog.FileName;
-                    string txtFilePath = csvFilePath.Replace(".csv", "_csv.txt");
-
-                    // Nacteni konfiguracniho souboru, pokud existuje
-                    OscilloscopeConfig config = new OscilloscopeConfig();
-                    if (File.Exists(txtFilePath)) {  // Opraveno - File je ted rozpoznano
-                        config.LoadTxtFile(txtFilePath);
-
-                        // Aktualizace informaci o osciloskopu v GUI
-                        OscilloscopeInfo.Text = $"Model: {config.Model}, Vzorkovaní: {config.SamplingRate} Sa/s, Časový rozsah: {config.TimeScale}s";
-                    } else {
-                        OscilloscopeInfo.Text = "Konfiguracni soubor nenalezen.";
                     }
 
                     // Vymazani predchoziho grafu
                     plot.Plot.Clear();
 
-                    // Vykresleni vsech signalu pomoci Add.Signal()
-                    foreach (var channel in loader.SignalData) {
-                        List<double> voltage = channel.Value.Select(v => v.Item2).ToList();
-                        double sampleRate = 1.0 / loader.TimeIncrement;
+                    int channelIndex = 0;
+                    double verticalOffset = 2.0; // Posun mezi signaly pro prehlednost
 
-                        var signalPlot = plot.Plot.Add.Signal(voltage.ToArray(), sampleRate);
-                        signalPlot.LegendText = channel.Key;
+                    // Pridani signalu do grafu
+                    foreach (var channel in loader.SignalData) {
+                        string channelName = channel.Key;
+                        double[] times = channel.Value.Select(v => v.Item1).ToArray();
+                        double[] voltages = channel.Value.Select(v => v.Item2).ToArray();
+
+                        // Posun signalu na ose Y, aby nebyly prekryte
+                        double[] adjustedVoltages = voltages.Select(v => v + channelIndex * verticalOffset).ToArray();
+
+                        // Pokud je signal binarni, pouzij Add.SignalXY() (step-like vykresleni)
+                        if (voltages.Distinct().Count() <= 3) { 
+                            plot.Plot.Add.SignalXY(times, adjustedVoltages);
+                        }
+                        else {
+                            plot.Plot.Add.Scatter(times, adjustedVoltages);
+                        }
+
+                        channelIndex++;
                     }
 
-                    plot.Plot.Legend.IsVisible = true;
+                    // Nastaveni vzhledu grafu
+                    plot.Plot.Title("Osciloskopicky signal");
+                    plot.Plot.Axes.AutoScale();
+                    plot.Plot.ShowLegend();
+
+                    // Obnoveni vykresleni grafu
                     plot.Refresh();
                 }
                 catch (Exception ex) {
